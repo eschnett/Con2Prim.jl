094305aaabea7d550468f144dd70867a356c2989
Documenter.jl <documenter@juliadocs.github.io>
2025-03-31 22:08:56 +0000

    build based on 475b706

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Con2Prim.jl","page":"Con2Prim.jl","title":"Con2Prim.jl","text":"","category":"section"},{"location":"","page":"Con2Prim.jl","title":"Con2Prim.jl","text":"Modules = [Con2Prim]","category":"page"}]
+}
